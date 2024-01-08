4ea348b2883dce97b84f58387d396a45550b28a5
JuanIgnacioMarchetto <tu@email.com>
2024-01-08 09:24:04 -0300

    8/1/2024-09:24

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import LookingForJob from './LookingForJob';
+import LookingForEmployee from './LookingForEmployee';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <button>Looking For Job</button>
-       <button>Looking For Employee</button>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/looking-for-job">
+            <button>Looking For Job</button>
+          </Link>
+          <Link to="/looking-for-employee">
+            <button>Looking For Employee</button>
+          </Link>
+
+          <Route path="/looking-for-job" component={LookingForJob} />
+          <Route path="/looking-for-employee" component={LookingForEmployee} />
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/LookingForJob.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LookingForJob() {
+  return (
+    <div>
+      <h1>Looking For Job Component</h1>
+    </div>
+  );
+}
+
+export default LookingForJob;
